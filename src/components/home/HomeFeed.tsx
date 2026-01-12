32c900533523db499a6a6ef600e1d15a6fd1c17e
devahmed1829 <roaan.dev@gmail.com>
2026-01-12 18:46:25 +0500

    Increase max width of HomeFeed container
    Changed the HomeFeed container's max width from 2xl to 5xl to allow more content to be displayed horizontally.

--- src/components/home/HomeFeed.tsx
+++ src/components/home/HomeFeed.tsx
@@ -97,7 +97,7 @@ export default function HomeFeed() {
     }
 
     return (
-        <div className="max-w-2xl mx-auto space-y-6">
+        <div className="max-w-5xl mx-auto space-y-6">
             {feedItems.map((item) => {
                 if (item.feedType === 'video') {
                     return (
